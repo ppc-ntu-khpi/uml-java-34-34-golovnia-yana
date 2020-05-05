c14d1c7c37b8eeeaff3d03af9b8f3d4bf86a90d7
Golovnia Yana <55803221+34-golovnia-yana@users.noreply.github.com>
2020-05-05 11:29:24 +0300

    Add files via upload

---
+++ src/restaurant/Admin.java
@@ -0,0 +1,167 @@
+package Admin;
+
+
+/**
+* @generated
+*/
+public class Admin {
+    
+    /**
+    * @generated
+    */
+    private Integer id;
+    
+    /**
+    * @generated
+    */
+    private String Firsname;
+    
+    /**
+    * @generated
+    */
+    private Integer phone;
+    
+    /**
+    * @generated
+    */
+    private String adress;
+    
+    /**
+    * @generated
+    */
+    private String lastName;
+    
+    
+    /**
+    * @generated
+    */
+    private Set<Order> order;
+    
+    /**
+    * @generated
+    */
+    private Set<Menu> menu;
+    
+    
+    /**
+    * @generated
+    */
+    public Integer getId() {
+        return this.id;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setId(Integer id) {
+        this.id = id;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getFirsname() {
+        return this.Firsname;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setFirsname(String Firsname) {
+        this.Firsname = Firsname;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer getPhone() {
+        return this.phone;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setPhone(Integer phone) {
+        this.phone = phone;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getAdress() {
+        return this.adress;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setAdress(String adress) {
+        this.adress = adress;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getLastName() {
+        return this.lastName;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public Order getOrder() {
+        return this.order;
+    }
+    
+    /**
+    * @generated
+    */
+    public Order setOrder(Order order) {
+        this.order = order;
+    }
+    
+    /**
+    * @generated
+    */
+    public Menu getMenu() {
+        return this.menu;
+    }
+    
+    /**
+    * @generated
+    */
+    public Menu setMenu(Menu menu) {
+        this.menu = menu;
+    }
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public getLastName() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getFirstName() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getId() {
+        //TODO
+    }
+    
+}
---
+++ src/restaurant/Menu.java
@@ -0,0 +1,148 @@
+package Menu;
+
+
+/**
+* @generated
+*/
+public class Menu {
+    
+    /**
+    * @generated
+    */
+    private Integer id;
+    
+    /**
+    * @generated
+    */
+    private String name;
+    
+    /**
+    * @generated
+    */
+    private String description;
+    
+    /**
+    * @generated
+    */
+    private Integer price;
+    
+    
+    /**
+    * @generated
+    */
+    private Set<Order> order;
+    
+    /**
+    * @generated
+    */
+    private Admin admin;
+    
+    
+    /**
+    * @generated
+    */
+    public Integer getId() {
+        return this.id;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setId(Integer id) {
+        this.id = id;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getName() {
+        return this.name;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setName(String name) {
+        this.name = name;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getDescription() {
+        return this.description;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setDescription(String description) {
+        this.description = description;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer getPrice() {
+        return this.price;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setPrice(Integer price) {
+        this.price = price;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public Order getOrder() {
+        return this.order;
+    }
+    
+    /**
+    * @generated
+    */
+    public Order setOrder(Order order) {
+        this.order = order;
+    }
+    
+    /**
+    * @generated
+    */
+    public Admin getAdmin() {
+        return this.admin;
+    }
+    
+    /**
+    * @generated
+    */
+    public Admin setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public update() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getInfo() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public create() {
+        //TODO
+    }
+    
+}
